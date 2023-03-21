325c64f08e83c6d92f5eb046aed26cccbd1ea083
FRANCISKA TOTH <totu.mihaela@yahoo.com>
2023-03-21 12:08:55 +0100

    AC 2 - Finished

--- src/test/java/com/upgenix/step_definitions/LogOut_StepDefinition.java
+++ src/test/java/com/upgenix/step_definitions/LogOut_StepDefinition.java
@@ -2,11 +2,18 @@ package com.upgenix.step_definitions;
 
 import com.upgenix.pages.LogInFunctionality;
 import com.upgenix.pages.LogOutFunctionality;
+import com.upgenix.utilies.Driver;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class LogOut_StepDefinition {
 
     LogOutFunctionality logOutFunctionality = new LogOutFunctionality();
+
+    Actions actions = new Actions(Driver.getDriver());
     @Then("user click to his own username")
     public void user_click_to_his_own_username() {
         logOutFunctionality.userNameButton.click();
@@ -18,4 +25,19 @@ public class LogOut_StepDefinition {
 
     }
 
+    @And("User clicks the step backButton after successfully logged out.")
+    public void userClicksTheStepBackButtonAfterSuccessfullyLoggedOut() {
+
+        Driver.getDriver().navigate().back();
+
+
+    }
+
+    @And("user can not go to homepage again.")
+    public void userCanNotGoToHomepageAgain() {
+        String expectedTitle = "Login | Best solution for startups";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains("Login |"));
+
+    }
 }
--- src/test/resources/features/Logout.feature
+++ src/test/resources/features/Logout.feature
@@ -11,3 +11,17 @@ Feature: As a user, I should be able to log out.
     Then user click to his own username
     And  user click to Log Out button
     Then User in on log in page
+
+
+  @UPGNX10-269
+  Scenario: AC2- The user can not go to the home page again by clicking the step back button after successfully logged out.
+    When User in on log in page
+    Then User enters "salesmanager15@info.com" address
+    And user enter "salesmanager"
+    Then user clicks on the log in button
+    And user should be on the homepage
+    Then user click to his own username
+    And  user click to Log Out button
+    Then User in on log in page
+    And User clicks the step backButton after successfully logged out.
+    And user can not go to homepage again.
